2a67a45a29c3a5ff1b85e00747cacae18e6167a8
Dig-Doug <douglas.roeper@gmail.com>
2015-10-31 15:16:04 -0400

    Added rapidjson lib

--- ScareBearPi/ScareBearPi/main.cpp
+++ ScareBearPi/ScareBearPi/main.cpp
@@ -11,9 +11,15 @@
 #include <sys/types.h>
 #include <sys/select.h>
 #include <sys/socket.h>
-#include "microhttpd.h"
 
+#include "microhttpd.h"
 #define PORT 8888
+
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace rapidjson;
 
 int answer_to_connection (void *cls, struct MHD_Connection *connection,
                           const char *url,
@@ -35,6 +41,26 @@ int answer_to_connection (void *cls, struct MHD_Connection *connection,
     return ret;
 }
 
+void jsonTest()
+{
+    // 1. Parse a JSON string into DOM.
+    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    Document d;
+    d.Parse(json);
+    
+    // 2. Modify it by DOM.
+    Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+    
+    // 3. Stringify the DOM
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+    
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << buffer.GetString() << std::endl;
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -46,6 +72,8 @@ int main(int argc, const char * argv[]) {
     daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,
                                &answer_to_connection, NULL, MHD_OPTION_END);
     if (NULL == daemon) return 1;
+    
+    jsonTest();
     
     getchar ();
     
