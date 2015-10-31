e68a0d24bc325eb1ac3d7a7c8fd6ce6b61b1e093
Dig-Doug <douglas.roeper@gmail.com>
2015-10-31 15:52:02 -0400

    Web server with json test

--- ScareBearPi/ScareBearPi/main.cpp
+++ ScareBearPi/ScareBearPi/main.cpp
@@ -21,13 +21,41 @@
 
 using namespace rapidjson;
 
+std::string jsonTest()
+{
+    // 1. Parse a JSON string into DOM.
+    //const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    Document d;
+    d.Parse("{}");
+
+    Value name;
+    name.SetString("Doug R.");
+    d.AddMember("Name", name, d.GetAllocator());
+    
+    Value age;
+    age.SetInt(19);
+    d.AddMember("Age", age, d.GetAllocator());
+    
+    Value email;
+    email.SetString("email@test.com");
+    d.AddMember("Email", email, d.GetAllocator());
+    
+    // 3. Stringify the DOM
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+    
+    // Output {"project":"rapidjson","stars":11, etc...}
+    return buffer.GetString();
+}
+
 int answer_to_connection (void *cls, struct MHD_Connection *connection,
                           const char *url,
                           const char *method, const char *version,
                           const char *upload_data,
                           size_t *upload_data_size, void **con_cls)
 {
-    const char *page  = "<html><body>Hello, browser!</body></html>";
+    const char *page  = jsonTest().c_str();
     
     struct MHD_Response *response;
     int ret;
@@ -41,30 +69,10 @@ int answer_to_connection (void *cls, struct MHD_Connection *connection,
     return ret;
 }
 
-void jsonTest()
-{
-    // 1. Parse a JSON string into DOM.
-    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
-    Document d;
-    d.Parse(json);
-    
-    // 2. Modify it by DOM.
-    Value& s = d["stars"];
-    s.SetInt(s.GetInt() + 1);
-    
-    // 3. Stringify the DOM
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    d.Accept(writer);
-    
-    // Output {"project":"rapidjson","stars":11}
-    std::cout << buffer.GetString() << std::endl;
-}
-
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Web server up!\n";
     
     
     struct MHD_Daemon *daemon;
